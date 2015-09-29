7e71679f9f4cb716e7152e3760d4cafe0f8208d2
Vladimir Krivenko <voffkaa@gmail.com>
2015-09-29 23:55:00 +0300

    Added app.css

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,3 +1,5 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 
 var envSuffix = process.env.NODE_ENV ? '.' + process.env.NODE_ENV : '.';
@@ -20,24 +22,45 @@ module.exports.output = {
 };
 
 module.exports.entry = {
-    dashboard: 'dashboard/logout.js',
+    dashboard: [
+        'dashboard/logout.js',
+        'dashboard/navbar.jsx'
+    ],
     vendors: [
-        'jquery',
         'underscore',
-        'backbone'
+        'backbone',
+        'react',
+        'react-bootstrap'
     ]
 };
 
 module.exports.plugins = [
-    new CommonsChunkPlugin('vendors', 'vendors.js')
+    new CommonsChunkPlugin('vendors', 'vendors.js'),
+    new ExtractTextPlugin('app.css', { allChunks: true })
 ];
 
 module.exports.module.loaders = [
     {
-        test: /\.js?$/,
+        test: /\.(jsx|js)$/,
         loader: 'babel-loader',
         exclude: /(node_modules)/
+    },
+    {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+    },
+    {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff&name=fonts/[name].[ext]'
+    },
+    {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
     }
+
 ];
 
 module.exports.resolve.root = [__dirname + '/frontApp'];
+module.exports.resolve.alias = {
+    'react': __dirname + '/node_modules/react'
+};
